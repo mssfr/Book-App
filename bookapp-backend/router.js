5e48ecf9d85b867cf429be72aceaf0a709713d9f
mssfr <89845848+mssfr@users.noreply.github.com>
2021-10-20 00:29:01 +0530

    Delete router.js

--- bookapp-backend/router.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const bookrouter = require("./bookschema");
-
-router.get("/",(req,res)=>{
-    res.json("router working")
-})
-module.exports = router;
